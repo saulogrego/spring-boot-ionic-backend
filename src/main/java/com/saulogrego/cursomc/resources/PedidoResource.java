c6bda8076683e60ddae5eb16ee04d28c3992c058
Saulo Grego <saulogrego@gmail.com>
2018-06-05 14:18:16 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/saulogrego/cursomc/resources/PedidoResource.java
+++ src/main/java/com/saulogrego/cursomc/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.saulogrego.cursomc.domain.Categoria;
 import com.saulogrego.cursomc.domain.Pedido;
-import com.saulogrego.cursomc.dto.CategoriaDTO;
 import com.saulogrego.cursomc.services.PedidoService;
 
 @RestController

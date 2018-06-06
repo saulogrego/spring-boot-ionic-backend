89fbcaaf73867e716a2eb40e2e322bd7ad7320ac
Saulo Grego <saulogrego@gmail.com>
2018-06-06 15:46:20 -0300

    Padronizando formato das exceções

--- src/main/java/com/saulogrego/cursomc/config/SecurityConfig.java
+++ src/main/java/com/saulogrego/cursomc/config/SecurityConfig.java
@@ -44,8 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
-			"/estados/**",
-			"/cidades/**"
+			"/estados/**"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
